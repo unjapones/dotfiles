0cbee46d3723ce5c3503d72298d5898683d11035
japones <japones@neeerd.NOGA>
2013-06-28 15:32:02 -0300

    Added awesome startup script (modify /usr/share/xsessions/awesome.desktop accordingly).

---
+++ local/awesome.sh
@@ -0,0 +1,8 @@
+# Fixing JAVA display problems
+_JAVA_AWT_WM_NONREPARENTING=1
+#compton -cCGb &
+/home/japones/Nerd/linux/desktop/compton/compton &
+gnome-settings-daemon &
+#fdpowermon &
+#parcellite --no-icon &
+exec awesome
