34466021052a2f0b01945cf2165d753ae86186fa
Jorge L. Shirai <jorgeshirai@gmail.com>
2015-03-03 20:29:47 -0300

    awesome: unbinds M-S-o

--- config/awesome/bindings.lua
+++ config/awesome/bindings.lua
@@ -88,7 +88,7 @@ globalkeys = awful.util.table.join(
     -- Dropdown terminal
     -- 24/02/2015 - Replace with the usage of terminator
     -- awful.key({ modkey            }, "o",      function () scratch.drop("xterm -bg '#212121' -ls -e 'tmux'","bottom","center",1,0.4) end),
-    awful.key({ modkey, "Shift"   }, "o",      function () scratch.drop("gnome-terminal","bottom","center",1,0.5) end),
+    -- awful.key({ modkey, "Shift"   }, "o",      function () scratch.drop("gnome-terminal","bottom","center",1,0.5) end),
     awful.key({ modkey,           }, "p",
               function () 
                   cheeky.util.switcher({
